0d66bc0a09e66f7ab6d4d6c58d436d84fc2ef213
zqb <734057028@qq.com>
2018-04-26 16:57:56 +0800

    1.0.0 1011 组件化IM模块创建

---
+++ component-ssdsim/src/main/runalone/java/com/dashi1314/ssdsim/SsdsImApplication.java
@@ -0,0 +1,11 @@
+package com.dashi1314.ssdsim;
+
+import android.app.Application;
+
+public class SsdsImApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
---
+++ component-ssdsim/src/main/runalone/java/com/dashi1314/ssdsim/SsdsImMainActivity.java
@@ -0,0 +1,33 @@
+package com.dashi1314.ssdsim;
+
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+
+import com.blankj.utilcode.util.AppUtils;
+import com.dashi1314.common.base.SimpleActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class SsdsImMainActivity extends SimpleActivity {
+    @BindView(R.id.toolbar)
+    Toolbar mToolbar;
+    @BindView(R.id.layout_load_fragment)
+    FrameLayout mLayoutLoadFragment;
+    @BindView(R.id.tv_title)
+    TextView mTvTitle;
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_runalone;
+    }
+
+    @Override
+    protected void initEventAndData() {
+        setSupportActionBar(mToolbar);
+        mTvTitle.setText("随身大师-IM模块");
+    }
+
+}
