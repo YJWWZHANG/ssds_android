bbddfac5bb4e51fa733cc7865c41011c999222af
zqb <734057028@qq.com>
2018-05-22 18:56:43 +0800

    1.0.0 16

--- app/src/main/java/com/dashi1314/ssds/mvp/ui/fragment/SsdsHomeMainFragment.java
+++ app/src/main/java/com/dashi1314/ssds/mvp/ui/fragment/SsdsHomeMainFragment.java
@@ -180,17 +180,18 @@ public class SsdsHomeMainFragment extends SimpleFragment {
         oks.disableSSOWhenAuthorize();
 
         // title标题，微信、QQ和QQ空间等平台使用
-        oks.setTitle("分享");
+        oks.setTitle("随身大师App");
         // titleUrl QQ和QQ空间跳转链接
-        oks.setTitleUrl("http://sharesdk.cn");
+        oks.setTitleUrl("https://www.dashi1314.com/");
         // text是分享文本，所有平台都需要这个字段
-        oks.setText("我是分享文本");
+        oks.setText("命理真综，面相恒真");
         // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
-        oks.setImagePath("/sdcard/test.jpg");//确保SDcard下面存在此张图片
+        oks.setImagePath("/sdcard/test.png");//确保SDcard下面存在此张图片
+//        oks.setImagePath("file:///android_asset/test.png");//确保SDcard下面存在此张图片
         // url在微信、微博，Facebook等平台中使用
-        oks.setUrl("http://sharesdk.cn");
+        oks.setUrl("https://www.dashi1314.com/");
         // comment是我对这条分享的评论，仅在人人网使用
-        oks.setComment("我是测试评论文本");
+        oks.setComment("欢迎来到随身大师");
         // 启动分享GUI
         oks.show(Utils.getApp());
     }
